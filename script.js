b43b17ad0ea58c34f9970ad10547469187cd244f
Thaís Alessandra <101782983+Thaisalessandra@users.noreply.github.com>
2023-06-04 23:42:45 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+alert("Em Construção")
+document.getElementById("1").addEventListener("click",abrir);
+function abrir(){
+    
+    modal.classList.toggle("hide");
+    fade.classList.toggle("hide");
+       
+}
+function fechar(){
+    modal.classList.toggle("hide");
+    fade.classList.toggle("hide");
+}
